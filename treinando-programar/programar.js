b5e16ccaf13d259100b844da1db5a180511d8a93
Your Name <you@example.com>
2022-05-29 21:38:11 -0300

    Treinando mais um pouco

---
+++ treinando-programar/programar.js
@@ -0,0 +1,52 @@
+console.log("Olá Melissa")
+console.log("Melissa meu amor maior saudades")
+console.log("esse Corinthians só me deixa com raiva")
+//vamos pular essa linha
+
+
+//Vamos pular mais essa linha
+
+//Atribuição de valores
+ nome = 'Otacilio'
+ sobrenome = 'Cunha'
+ idade = 37
+ altura = 1.82
+ cidade = "caxambu"
+ email = true
+
+console . log ('nome:', nome)
+console . log ('idade:', idade)
+console . log ('cidade:', cidade)
+console . log ('email:', email)
+console . log ('altura:', altura)
+
+let nota1
+let nota2
+
+nota1 = 8
+nota2 = 10
+
+console.log(nota1 + nota2)
+let media = (nota1 + nota2) /2
+
+console.log('media:', media)
+
+let resultado = 100 + 840
+console.log('resultado:', resultado)
+
+//condicionais
+let idadeDoAluno = 21
+if (idadeDoAluno > 20){
+    //se for verdadeiro
+    console.log('Apto a cursa a faculdade')
+} else{
+    //se for falso
+    console.log('Não apto a cursa a facudade')
+}
+
+
+
+
+
+
+
